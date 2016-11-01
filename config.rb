1c06ce5c21c2bef726df415b071be4e588939317
Ethan Lin <ethan.lin@uwex.edu>
2016-11-01 13:25:46 -0500

    added breakpoint as a bower component

--- config.rb
+++ config.rb
@@ -1,17 +1,16 @@
 require 'compass/import-once/activate'
 # Require any additional compass plugins here.
-require 'breakpoint'
+add_import_path 'bower_components/breakpoint-sass/stylesheets'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "build/assets/css/"
-sass_dir = "scss/"
-images_dir = "build/assets/images/"
-javascripts_dir = "build/assets/js/"
-fonts_dir = "build/assets/font/"
+sass_dir = "source/assets/scss/"
+images_dir = "source/assets/images/"
+fonts_dir = "source/assets/font/"
 
-output_style = :expanded
-environment = :development
+output_style = :nested
+environment = :production
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
