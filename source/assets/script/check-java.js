2c497e63ab6fa06ff53b17b5034f1124c6df1551
Ethan Lin <ethan.lin@uwex.edu>
2016-11-14 22:29:53 -0600

    rephrased Java check message

--- source/assets/script/check-java.js
+++ source/assets/script/check-java.js
@@ -29,7 +29,34 @@ ready( function() {
     
     systemRC.appendChild( node );
     
-    if ( installedVersion === undefined || installedVersion.length === 0 ) {
+    if ( installedVersion[0] !== undefined ) {
+        
+        var installed = installedVersion[0].replace(/[^0-9]/gi, '');
+        var target = checkVersion.replace(/[^0-9]/gi, '');
+        
+        if ( installed >= target ) {
+    
+            if ( node.classList ) {
+                node.classList.add( 'success' );
+            } else {
+                node.className += ' success';
+            }
+            
+            node.innerHTML = "<p><span class=\"icon-checkmark big green\"></span><span class=\"icon-java big\"></span><strong>Java (" + installedVersion[0] + ") is installed!</strong></p><p class='notes'><strong>Notes:</strong> for Safari users, Java may not be enabled even it is installed. Please double check your Safari Preferences.</p>";
+            
+        } else {
+        
+            if ( node.classList ) {
+                node.classList.add( 'warning' );
+            } else {
+                node.className += ' warning';
+            }
+            
+            node.innerHTML = "<p><span class=\"icon-warning big yellow\"></span><span class=\"icon-java big\"></span><strong>Java (" + installedVersion[0] + ") is outdated! - <span class=\"warning\">UPDATE REQUIRED</span></strong></p><p>Java version <strong>" + checkVersion + " or greater</strong> is required. Please update <a href=\"http://java.com/en/download/help/index_installing.xml\" target=\"_blank\">Java</a><span class=\"icon-link\"></span>.<p class='notes'><strong>Note:</strong> Java 7 (version 1.7.0 or greater) is not supported by 32-bit Google Chrome.</p>";
+            
+        }
+        
+    } else {
         
         if ( node.classList ) {
             node.classList.add( 'danger' );
@@ -37,27 +64,7 @@ ready( function() {
             node.className += ' danger';
         }
         
-        node.innerHTML = "<p><span class=\"icon-danger big red\"></span><span class=\"icon-java big\"></span><strong>Java is not installed or enabled!</strong></p><p>Java version <strong>" + checkVersion + " or greater</strong> is required. Please <a href=\"http://java.com/en/download/help/index_installing.xml\" target=\"_blank\">install</a><span class=\"icon-link\"></span> or <a href=\"http://java.com/en/download/help/enable_browser.xml\" target=\"_blank\">enable</a><span class=\"icon-link\"></span> Java.<br /><small><strong>Note:</strong> Java 7 (version 1.7.0 or greater) is not supported by 32-bit Google Chrome.</small></p>";
-        
-    } else if ( installedVersion[0] >= checkVersion.toString() ) {
-    
-        if ( node.classList ) {
-            node.classList.add( 'success' );
-        } else {
-            node.className += ' success';
-        }
-        
-        node.innerHTML = "<p><span class=\"icon-checkmark big green\"></span><span class=\"icon-java big\"></span><strong>Java (" + installedVersion[0] + ") is enabled!</strong></p>";
-        
-    } else {
-    
-        if ( node.classList ) {
-            node.classList.add( 'warning' );
-        } else {
-            node.className += ' warning';
-        }
-        
-        node.innerHTML = "<p><span class=\"icon-warning big yellow\"></span><span class=\"icon-java big\"></span><strong>Java (" + installedVersion + ") is outdated! - <span class=\"warning\">UPDATE REQUIRED</span></strong></p><p>Java version <strong>" + checkVersion + " or greater</strong> is required. Please update <a href=\"http://java.com/en/download/help/index_installing.xml\" target=\"_blank\">Java</a><span class=\"icon-link\"></span>.<br /><small><strong>Note:</strong> Java 7 (version 1.7.0 or greater) is not supported by 32-bit Google Chrome.</small>";
+        node.innerHTML = "<p><span class=\"icon-danger big red\"></span><span class=\"icon-java big\"></span><strong>Java is not installed or enabled!</strong></p><p>Java version <strong>" + checkVersion + " or greater</strong> is required. Please <a href=\"http://java.com/en/download/help/index_installing.xml\" target=\"_blank\">install</a><span class=\"icon-link\"></span> or <a href=\"http://java.com/en/download/help/enable_browser.xml\" target=\"_blank\">enable</a><span class=\"icon-link\"></span> Java.</p><p class='notes'><strong>Note:</strong> Java 7 (version 1.7.0 or greater) is not supported by 32-bit Google Chrome.</p>";
         
     }
     
